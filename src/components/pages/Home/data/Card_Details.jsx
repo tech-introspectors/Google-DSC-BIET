ffdef90e607235d75e3be9f765aa8ec429b8b153
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-10-05 03:20:08 +0530

    added card section

--- src/components/pages/Home/data/Card_Details.jsx
+++ src/components/pages/Home/data/Card_Details.jsx
@@ -1,27 +1,44 @@
+import img1 from "../../../../asserts/responsive.svg";
+import img2 from "../../../../asserts/android.svg";
+import img3 from "../../../../asserts/android.svg";
+import img4 from "../../../../asserts/robot.svg";
+import img5 from "../../../../asserts/python.svg";
+import img6 from "../../../../asserts/javascript.svg";
+import img7 from "../../../../asserts/responsive.svg";
+import img8 from "../../../../asserts/java.svg";
+
 const Card_Details = [
   {
-    img: "",
+    img: img1,
     tech_Name: "Website Development",
   },
   {
-    img: "",
+    img: img2,
     tech_Name: "Android App Development",
   },
   {
-    img: "",
+    img: img3,
     tech_Name: "Flutter App Development",
   },
   {
-    img: "",
+    img: img4,
     tech_Name: "Machine Learning",
   },
   {
-    img: "",
+    img: img5,
+    tech_Name: "Python",
+  },
+  {
+    img: img6,
     tech_Name: "JavaScript",
   },
   {
-    img: "",
+    img: img7,
     tech_Name: "React js",
+  },
+  {
+    img: img8,
+    tech_Name: "Java",
   },
 ];
 
