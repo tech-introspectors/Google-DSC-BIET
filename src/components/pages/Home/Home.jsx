ce2a93ce51767684ae063bde8a6c863877a77d7e
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-10-05 12:23:29 +0530

    Added recent-section

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -4,7 +4,11 @@ import TitleCenter from "./components/TitleCenter";
 import Card from "./components/Card";
 import Card_Details from "./data/Card_Details";
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./data/Data";
-import { TitleCenter1 } from "./data/TitleCenterData";
+import {
+  TitleCenter1,
+  TitleCenter2,
+  TitleCenter3,
+} from "./data/TitleCenterData";
 
 import "../Home/Home.css";
 
@@ -18,7 +22,10 @@ export default function Home() {
           <HeroSection {...homeObjThree} />
           <HeroSection {...homeObjFour} />
         </div>
-        <div className="tech_bg_color  ">
+
+        {/* Technologies section */}
+
+        <div className="tech_bg_color">
           <div className="container">
             <div className="text-center">
               <TitleCenter {...TitleCenter1} />
@@ -71,6 +78,51 @@ export default function Home() {
                   img={Card_Details[7].img}
                   tech_Name={Card_Details[7].tech_Name}
                 />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Technologies section ended */}
+
+        {/* recent_project section */}
+
+        <div className="text-center recent_project">
+          <TitleCenter {...TitleCenter2} />
+          <p style={{ color: "grey" }}>No Projects Available Right Now</p>
+        </div>
+
+        {/* recent_project section ended */}
+
+        <div className="form_bg_color">
+          <div className="container ">
+            <div className="row">
+              <div className="col-6">
+                <h1>Have an Awesome Idea, Lets BrainStorm</h1>
+                <p className="mb-1">
+                  <b>Input:</b>
+                </p>
+                <p className="mb-1">
+                  const <b>idea</b> = ["Revolutionary Idea"];
+                </p>
+                <p className="mb-1">
+                  if(
+                  <b>
+                    ! idea.includes("technicalKnowledge" || "developers" ||
+                    {"  "}"domainKnowledge" || "designers "{" "}
+                  </b>
+                  ){" { "}
+                </p>
+                <p className="ml-4">
+                  <b>submitIdea();</b>
+                </p>
+                <p className="mb-1">{" } "}</p>
+                <p className="mb-1">
+                  <b>Output: </b>helpReceived()
+                </p>
+              </div>
+              <div className="col-6">
+                <div className="row form_style"></div>
               </div>
             </div>
           </div>
