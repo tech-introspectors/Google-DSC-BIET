fa961fbccd437a64bf04e98c528f12140dec8273
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-10-19 18:01:56 +0530

    Added project page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -7,20 +7,20 @@ export default function Projects() {
   return (
     <>
        <div id = "egg" >
-      <div className="container contact_container" id = "star-five ">
-        <h2 className = "text-center custom_heading">Our Projects</h2>
-      <div>  
-      <div className = "btn_custom_container">
-        <Button 
-          text = "Create Project"
-          column = "2"
-          buttonSize = '50px'
-          buttonWidth = '170px'
-          borderRadius = "5px"
-          btn_customStyle = "btn_customs"
-         />
-         </div>
-      </div>
+        <div className="container contact_container" id = "star-five ">
+          <h2 className = "text-center custom_heading">Our Projects</h2>
+           
+          <div className = "btn_custom_container">
+            <Button 
+              text = "ADD  YOUR PROJECT"
+              column = "2"
+              buttonSize = '50px'
+              buttonWidth = '250px'
+              borderRadius = "5px"
+              btn_customStyle = "btn_customs"
+            />
+           
+        </div>
       </div>
       
       <Footer />
