4420fa74ff3c1d0e13ce6cb0e13647b328f3db51
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-10-24 02:53:04 +0530

    Added event page

---
+++ src/pages/Events/components/Events_Card.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Button from "../../Home/components/Button";
+import { Link } from "react-router-dom";
+import "../Event.css";
+
+export default function Events_Card(props) {
+  const [completed, setcompleted] = useState(false);
+  const OnClickButton = () => {
+    setcompleted(!completed);
+  };
+
+  return (
+    <>
+      <div className="event_container">
+        <div className="eventDetail_container">
+          <img src={props.template} alt={props.event_name} />
+          <div className="event_details">
+            <h6>Events On : {props.events_on}</h6>
+            <div className="row">
+              <Link className="link" to={props.eventLink}>
+                <h3>{props.event_name}</h3>
+              </Link>
+
+              <Button
+                text={completed ? "Completed" : "Not completed"}
+                borderRadius="15px"
+                handleOnClick={OnClickButton}
+                btn_customStyle={`${
+                  completed ? "btn-danger" : "btn-primar"
+                } btn_style`}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Team/components/Team_Members.jsx
+++ src/pages/Team/components/Team_Members.jsx
@@ -1,45 +1,44 @@
-import React from 'react';
-import { FaFacebookF, FaTwitter, FaInstagram, FaGithub  } from 'react-icons/fa';
+import React from "react";
+import { FaFacebookF, FaTwitter, FaInstagram, FaGithub } from "react-icons/fa";
 
 const Team_Members = (props) => {
-    return (
-        <>
-                <div className = "core_team_container"> 
-                        <div className="members_container">
-                            <img src = {props.img} alt = "" />
-                            <div className = "member_details">
-                                <h3>{props.name}</h3>
-                                <p>{props.profession}</p>
-                            </div>  
-                            <div className = "d-flex justify-content-center">
-                        <div>
-                            <a className = "social_media" href="#" target = "_black">
-                                <FaFacebookF  className = "icon"/>
-                            </a>
-                        </div>
+  return (
+    <>
+      <div className="core_team_container">
+        <div className="members_container">
+          <img src={props.img} alt="" />
+          <div className="member_details">
+            <h3>{props.name}</h3>
+            <p>{props.profession}</p>
+          </div>
+          <div className="d-flex justify-content-center">
+            <div>
+              <a className="social_media" href="#" target="_black">
+                <FaFacebookF className="icon" />
+              </a>
+            </div>
 
-                        <div>
-                            <a className = "social_media" href="#" target = "_black">
-                                <FaTwitter  className = "icon" />
-                            </a>
-                        </div>
+            <div>
+              <a className="social_media" href="#" target="_black">
+                <FaTwitter className="icon" />
+              </a>
+            </div>
 
-                        <div>
-                            <a className = "social_media" href="#" target = "_black">
-                                <FaInstagram  className = "icon" />
-                            </a>
-                        </div>
-                        <div>
-                            <a className = "social_media" href="#" target = "_black">
-                                <FaGithub  className = "icon" />
-                            </a>
-                        </div>
-                    </div>
-                        </div>
-
-                </div>
-        </>
-    );
-}
+            <div>
+              <a className="social_media" href="#" target="_black">
+                <FaInstagram className="icon" />
+              </a>
+            </div>
+            <div>
+              <a className="social_media" href="#" target="_black">
+                <FaGithub className="icon" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Team_Members;
