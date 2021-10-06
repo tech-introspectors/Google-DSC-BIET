06e891e7e632992018d4583d276c5cf18c108627
Sai kamal <65682028+Dantusaikamal@users.noreply.github.com>
2021-10-06 13:42:30 +0530

    Create MembersData.jsx

--- src/pages/Team/Team_data/MembersData.jsx
+++ src/pages/Team/Team_data/MembersData.jsx
@@ -1,17 +1,43 @@
-import img from "../../../assets/teamMember_img/pavan.jpeg";
-import img1 from "../../../assets/teamMember_img/kamal.jpeg";
+import pavan from "../../../assets/teamMember_img/Pavan.jpg";
+import kamal from "../../../assets/teamMember_img/kamal.jpg";
+import Rithvik from "../../../assets/teamMember_img/Rithvik.jpg";
+import Jahnavi from "../../../assets/teamMember_img/Jahnavi.jpg";
+import Tejaswi from "../../../assets/teamMember_img/Tejaswi.jpg";
+import Amaresh from "../../../assets/teamMember_img/Amaresh.jpg";
 
 const MembersData = [
   {
-    img: img1,
+    img: kamal,
     memberName: "Dantu Sai Kamal",
     profession: "Lead, GDSC BIET",
   },
   {
-    img: img,
+    img: pavan,
     memberName: "M. Pavan Kumar",
     profession: "Projects Lead",
   },
+
+  {
+    img: img,
+    memberName: "Pavan Rithvik",
+    profession: "Design team Lead",
+  },
+    {
+    img: img1,
+    memberName: "Jahnavi Manne",
+    profession: "Management Lead",
+  },
+  {
+    img: img,
+    memberName: "Eleswarapu Tejaswi",
+    profession: "Public Relations Lead",
+  },
+    {
+    img: img1,
+    memberName: "Amaresh Prasad",
+    profession: "Management Co-Lead",
+  },
+
 ];
 
 export default MembersData;
